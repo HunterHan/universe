7af32f43e22935bad8de199c86d389158deea71c
hanhongtao <784622876@qq.com>
2017-06-25 17:53:37 +0800

    thread-related test

---
+++ project_java_source_code/src/main/java/com/laiwu/source/code/thread/TestDeadLock.java
@@ -0,0 +1,52 @@
+package com.laiwu.source.code.thread;
+
+public class TestDeadLock implements Runnable {
+	/**
+	 * 当两个线程以相反的顺序持有两把锁时，就有可能产生死锁现象
+	 */
+	public int flag;
+	public static Object o1 = new Object();
+	public static Object o2 = new Object();
+
+	public static void main(String[] args) {
+		TestDeadLock td1 = new TestDeadLock();
+		TestDeadLock td2 = new TestDeadLock();
+		td1.flag = 1;
+		td2.flag = 0;
+		Thread t1 = new Thread(td1);
+		Thread t2 = new Thread(td2);
+		t1.start();
+		t2.start();
+	}
+
+	@Override
+	public void run() {
+		System.out.println("flag:" + flag);
+		if (flag == 1) {
+			synchronized (o1) {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				synchronized (o2) {
+					System.out.println("1");
+				}
+			}
+
+		} else if (flag == 0) {
+			synchronized (o2) {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				synchronized (o1) {
+					System.out.println("0");
+				}
+			}
+
+		}
+
+	}
+}
